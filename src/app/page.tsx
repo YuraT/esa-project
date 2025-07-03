07e65a8d0812a6fa69a444ebd3c880ceee1b9978
Eric Nguyen <eriicnguyen1@gmail.com>
2025-07-03 11:24:50 -0700

    created "pesticide info center" part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,33 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>ESA Project</h1>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+    <div className="items-center min-h-screen font-[family-name:var(--font-merriweather-sans)]">
+      <div className="relative">
+        <img
+          src={"pesticideuser.svg"}
+          alt="Pesticide User"
+          className="w-full h-auto"
+        ></img>
+        <div className="absolute inset-0 bg-gradient-to-r from-[#275C9D]/80 to-white/50"></div>
+        <div className="absolute top-[25%] left-[10%] w-[clamp(23rem,60vw,51rem)] px-[clamp(1rem,5vw,6rem)]">
+          <p className="font-bold text-[clamp(2rem,5vw,4rem)] pb-1">
+            Pesticide Information Center
+          </p>
+          <p className="font-bold text-[clamp(1rem,2.5vw,2rem)] text-white">
+            ESA rules, simplified.
+          </p>
+          <div className="absolute bottom-[-80%]">
+            <p className="text-[clamp(0.3rem,1vw,0.6rem)] text-[#9E9FA0]">
+              *This site does not store or collect personal data about pesticide
+              application*
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative h-24 bg-white"></div>
+
+      {/* <footer className="absolute row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <Link
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://blt.epa.gov/blt/swagger/"
@@ -53,7 +75,7 @@ export default function Home() {
           />
           EPA
         </Link>
-      </footer>
+      </footer> */}
     </div>
   );
 }
