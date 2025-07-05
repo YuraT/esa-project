034f0ccb5c18267ad62b96f28c435fa5e75a74ba
Eric Nguyen <eriicnguyen1@gmail.com>
2025-07-05 01:47:04 -0700

    fixed padding and removed footer comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,23 @@ import { FiUploadCloud, FiBookOpen } from "react-icons/fi";
 import { IoSearchSharp } from "react-icons/io5";
 
 export default function Home() {
+  // tools styling
   const grayTools =
     "font-bold text-white bg-[#ACADAA] rounded-3xl w-[clamp(5rem,11vw,15rem)] py-[clamp(0.5rem,1vw,3rem)] text-[clamp(0.5rem,1vw,5rem)] transition-colors hover:bg-[#C1C2BF]";
   const blueTools =
     "font-bold text-white bg-[#537BBA] rounded-2xl w-[clamp(8rem,20vw,20rem)] py-[clamp(0.5rem,1vw,3rem)] text-[clamp(0.8rem,2vw,2rem)] transition-colors hover:bg-[#648fd1]";
 
+  // icons styling
   const iconsSpacing = "flex items-start gap-x-[clamp(0.2rem,1vw,1rem)]";
   const iconsShape =
     "flex items-center justify-center bg-[#CEDEBD] w-[clamp(3rem,10vw,10rem)] h-[clamp(3rem,10vw,10rem)] rounded-full";
+  const iconsSize = "text-[clamp(1.5rem,4vw,4rem)] text-[#3E4651]";
+
+  // steps styling
   const steps =
     "font-bold text-[#3E4651] text-[clamp(1rem,2.5vw,2.5rem)] leading-tight";
   const stepsDesc =
     "font-normal text-[clamp(0.5rem,1.1vw,1.1rem)] w-[clamp(5rem,10vw,10rem)]";
-  const iconsSize = "text-[clamp(1.5rem,4vw,4rem)] text-[#3E4651]";
 
   return (
     <div className="items-center min-h-screen font-[family-name:var(--font-merriweather-sans)]">
@@ -23,10 +27,9 @@ export default function Home() {
         <img
           src={"pesticideuser.svg"}
           alt="Pesticide User"
-          className="w-full h-auto"
         ></img>
 
-        {/** gray search, map, manual entry buttons */}
+        {/* gray search, map, manual entry buttons */}
         <div className="absolute top-[5%] right-4 z-10">
           <div className="flex pr-[clamp(1rem,3vw,4rem)] gap-x-[clamp(0.2rem,1.5vw,2rem)]">
             <button className={grayTools}>CHAT</button>
@@ -35,7 +38,7 @@ export default function Home() {
           </div>
         </div>
 
-        {/** pesticide user image gradient */}
+        {/* pesticide user image gradient */}
         <div className="absolute inset-0 bg-gradient-to-r from-[#275C9D]/80 to-white/40"></div>
 
         <div className="absolute top-[25%] left-[10%] w-[clamp(23rem,60vw,51rem)] px-[clamp(1rem,5vw,6rem)]">
@@ -54,9 +57,9 @@ export default function Home() {
         </div>
       </div>
 
-      {/** get clear... */}
-      <div className="flex items-center relative h-[clamp(11rem,30vw,30rem)] bg-white">
-        <div className="pr-[clamp(2rem,5vw,5rem)] pl-[clamp(5rem,21vw,21rem)]">
+      {/* Get clear... */}
+      <div className="flex items-center bg-white">
+        <div className="pr-[clamp(2rem,5vw,5rem)] pl-[clamp(5rem,21vw,21rem)] py-[clamp(3rem,9vw,9rem)]">
           <p className="font-bold text-[#375B85] text-right text-[clamp(0.8rem,2.3vw,3rem)]">
             Get clear, customized{" "}
             <span className="text-[#537BBA]">ESA pesticide guidelines</span>{" "}
@@ -69,15 +72,15 @@ export default function Home() {
         </div>
       </div>
 
-      {/** how it works */}
-      <div className="relative bg-[#F0F0F0] h-[clamp(11rem,30vw,30rem)]">
-        <div className="flex justify-center py-[clamp(1rem,4vw,4rem)]">
-          <p className="font-bold text-[#375B85] text-[clamp(1rem,3vw,3rem)] ">
+      {/* How It Works. */}
+      <div className="bg-[#F0F0F0] py-[clamp(2rem,6vw,6rem)]">
+        <div className="flex justify-center">
+          <p className="font-bold text-[#375B85] text-[clamp(1rem,3vw,3rem)] pb-[clamp(1.3rem,4vw,4rem)]">
             How It Works.
           </p>
         </div>
         <div className="flex justify-center gap-x-[clamp(0.5rem,6vw,6rem)]">
-          {/** 1. Choose */}
+          {/* 1. Choose */}
           <div className={iconsSpacing}>
             <div className={iconsShape}>
               <FiUploadCloud className={iconsSize} />
@@ -92,7 +95,7 @@ export default function Home() {
               </p>
             </h1>
           </div>
-          {/** 2. Search */}
+          {/* 2. Search */}
           <div className={iconsSpacing}>
             <div className={iconsShape}>
               <IoSearchSharp className={iconsSize} />
@@ -108,7 +111,7 @@ export default function Home() {
               </p>
             </h1>
           </div>
-          {/** 3. Inform */}
+          {/* 3. Inform */}
           <div className={iconsSpacing}>
             <div className={iconsShape}>
               <FiBookOpen className={iconsSize} />
@@ -125,8 +128,8 @@ export default function Home() {
         </div>
       </div>
 
-      {/** start your search */}
-      <div className="flex flex-col items-center justify-center relative bg-[#275C9D] h-[clamp(15rem,35vw,35rem)]">
+      {/* Start Your Search. */}
+      <div className="flex flex-col items-center justify-center bg-[#275C9D] py-[clamp(2rem,5vw,5rem)]">
         <p className="font-bold text-white text-[clamp(1.5rem,3.5vw,3.5rem)]">
           Start Your Search.
         </p>
@@ -146,58 +149,8 @@ export default function Home() {
         </p>
       </div>
 
-      {/** footer */}
-      <div className="relative">
-        <Footer></Footer>
-      </div>
-
-      {/* <footer className="absolute row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <Link
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://blt.epa.gov/blt/swagger/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          BLT API
-        </Link>
-        <Link
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.epa.gov/pesticides/mitigation-menu"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Mitigation Menu
-        </Link>
-        <Link
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.epa.gov/endangered-species"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          EPA
-        </Link>
-      </footer> */}
+      {/* footer */}
+      <Footer></Footer>
     </div>
   );
 }
