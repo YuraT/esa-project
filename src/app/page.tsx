5883e8592a1b16ffa8444f062efc0631d94ae82d
Eric Nguyen <eriicnguyen1@gmail.com>
2025-07-17 01:19:03 -0700

    updated clamp sizing for min (mobile) and max (1920+)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,48 +7,48 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   // tools styling
   const grayTools =
-    "font-bold text-white bg-[#ACADAA] rounded-3xl w-[clamp(5rem,11vw,15rem)] py-[clamp(0.5rem,1vw,3rem)] text-[clamp(0.5rem,1vw,5rem)] transition-colors hover:bg-[#C1C2BF]";
+    "font-bold text-white bg-[#ACADAA] rounded-[clamp(0.75rem,2vw,4rem)] w-[clamp(0.1rem,11vw,18rem)] py-[clamp(0.3rem,1vw,3rem)] text-[clamp(0.2rem,1vw,2rem)] transition-colors hover:bg-[#C1C2BF]";
   const blueTools =
-    "font-bold text-white bg-[#537BBA] rounded-2xl w-[clamp(8rem,20vw,20rem)] py-[clamp(0.5rem,1vw,3rem)] text-[clamp(0.8rem,2vw,2rem)] transition-colors hover:bg-[#648fd1]";
+    "font-bold text-white bg-[#537BBA] rounded-[clamp(0.5rem,1.5vw,2rem)] w-[clamp(5rem,20vw,32rem)] py-[clamp(0.5rem,1vw,3rem)] text-[clamp(0.5rem,2vw,4rem)] transition-colors hover:bg-[#648fd1]";
 
   // icons styling
-  const iconsSpacing = "flex items-start gap-x-[clamp(0.2rem,1vw,1rem)]";
+  const iconsSpacing = "flex items-start gap-x-[clamp(0.1rem,1vw,2rem)]";
   const iconsShape =
-    "flex items-center justify-center bg-[#CEDEBD] w-[clamp(3rem,10vw,10rem)] h-[clamp(3rem,10vw,10rem)] rounded-full";
-  const iconsSize = "text-[clamp(1.5rem,4vw,4rem)] text-[#3E4651]";
+    "flex items-center justify-center bg-[#CEDEBD] w-[clamp(2rem,10vw,20rem)] h-[clamp(2rem,10vw,20rem)] rounded-full";
+  const iconsSize = "text-[clamp(0.8rem,4vw,8rem)] text-[#3E4651]";
 
   // steps styling
   const steps =
-    "font-bold text-[#3E4651] text-[clamp(1rem,2.5vw,2.5rem)] leading-tight";
+    "font-bold text-[#3E4651] text-[clamp(0.7rem,2.5vw,4rem)] leading-tight";
   const stepsDesc =
-    "font-normal text-[clamp(0.5rem,1.1vw,1.1rem)] w-[clamp(5rem,10vw,10rem)]";
+    "font-normal text-[clamp(0.25rem,1.1vw,3rem)] text-[#3E4651] w-[clamp(2.7rem,10vw,20rem)]";
 
   const router = useRouter();
+  const goToChat = () => router.push("/chat");
+  const goToMap = () => router.push("/map");
+  const goToManualEntry = () => router.push("/manual-entry");
 
   return (
-    <div className="items-center min-h-screen font-[family-name:var(--font-merriweather-sans)]">
+    <div className="bg-[#3E4651] items-center min-h-screen font-[family-name:var(--font-merriweather-sans)] overflow-x-hidden">
       <div className="relative">
-        <img src={"pesticideuser.svg"} alt="Pesticide User"></img>
+        <img
+          src="/pesticideuser.svg"
+          alt="Pesticide User"
+          className="w-full max-w-screen-3xl h-auto"
+        />
+
+        {/* <img src={"pesticideuser.svg"} alt="Pesticide User" fill={false}></img> */}
 
         {/* gray search, map, manual entry buttons */}
-        <div className="absolute top-[5%] right-4 z-10">
-          <div className="flex pr-[clamp(1rem,3vw,4rem)] gap-x-[clamp(0.2rem,1.5vw,2rem)]">
-            <button
-              className={grayTools}
-              onClick={() => router.push("/chat")}
-            >
+        <div className="absolute top-[5%] right-[0.01%] z-10">
+          <div className="flex pr-[clamp(0.3rem,3vw,6rem)] gap-x-[clamp(0.2rem,1.5vw,3rem)]">
+            <button className={grayTools} onClick={goToChat}>
               CHAT
             </button>
-            <button
-              className={grayTools}
-              onClick={() => router.push("/map")}
-            >
+            <button className={grayTools} onClick={goToMap}>
               MAP
             </button>
-            <button
-              className={grayTools}
-              onClick={() => router.push("/manual-entry")}
-            >
+            <button className={grayTools} onClick={goToManualEntry}>
               MANUAL ENTRY
             </button>
           </div>
@@ -57,15 +57,15 @@ export default function Home() {
         {/* pesticide user image gradient */}
         <div className="absolute inset-0 bg-gradient-to-r from-[#275C9D]/80 to-white/40"></div>
 
-        <div className="absolute top-[25%] left-[10%] w-[clamp(23rem,60vw,51rem)] px-[clamp(1rem,5vw,6rem)]">
-          <p className="font-bold text-[clamp(2rem,5vw,4rem)] pb-1">
+        <div className="absolute top-[25%] left-[10%] w-[clamp(12rem,60vw,90rem)] px-[clamp(1rem,5vw,6rem)]">
+          <p className="font-bold text-white text-[clamp(1rem,5vw,9rem)] pb-1">
             Pesticide Information Center
           </p>
-          <p className="font-bold text-[clamp(1rem,2.5vw,2rem)] text-white">
+          <p className="font-bold text-[clamp(0.6rem,2.5vw,5rem)] text-white">
             ESA rules, simplified.
           </p>
           <div className="absolute bottom-[-70%]">
-            <p className="text-[clamp(0.3rem,1vw,1rem)] text-[#9E9FA0]">
+            <p className="text-[clamp(0.2rem,1vw,2rem)] text-[#9E9FA0]">
               *This site does not store or collect personal data about pesticide
               application*
             </p>
@@ -75,8 +75,8 @@ export default function Home() {
 
       {/* Get clear... */}
       <div className="flex items-center bg-white">
-        <div className="pr-[clamp(2rem,5vw,5rem)] pl-[clamp(5rem,21vw,21rem)] py-[clamp(3rem,9vw,9rem)]">
-          <p className="font-bold text-[#375B85] text-right text-[clamp(0.8rem,2.3vw,3rem)]">
+        <div className="pr-[clamp(1rem,5vw,5rem)] pl-[clamp(4rem,21vw,23rem)] py-[clamp(1rem,9vw,12rem)]">
+          <p className="font-bold text-[#375B85] text-right text-[clamp(0.5rem,2.3vw,4rem)]">
             Get clear, customized{" "}
             <span className="text-[#537BBA]">ESA pesticide guidelines</span>{" "}
             based on your location with our easy-to use{" "}
@@ -89,13 +89,13 @@ export default function Home() {
       </div>
 
       {/* How It Works. */}
-      <div className="bg-[#F0F0F0] py-[clamp(2rem,6vw,6rem)]">
+      <div className="bg-[#F0F0F0] py-[clamp(1rem,6vw,8rem)]">
         <div className="flex justify-center">
-          <p className="font-bold text-[#375B85] text-[clamp(1rem,3vw,3rem)] pb-[clamp(1.3rem,4vw,4rem)]">
+          <p className="font-bold text-[#375B85] text-[clamp(0.8rem,3vw,5rem)] pb-[clamp(0.5rem,4vw,4rem)]">
             How It Works.
           </p>
         </div>
-        <div className="flex justify-center gap-x-[clamp(0.5rem,6vw,6rem)]">
+        <div className="flex justify-center gap-x-[clamp(0.5rem,6vw,10rem)]">
           {/* 1. Choose */}
           <div className={iconsSpacing}>
             <div className={iconsShape}>
@@ -145,37 +145,28 @@ export default function Home() {
       </div>
 
       {/* Start Your Search. */}
-      <div className="flex flex-col items-center justify-center bg-[#275C9D] py-[clamp(2rem,5vw,5rem)]">
-        <p className="font-bold text-white text-[clamp(1.5rem,3.5vw,3.5rem)]">
+      <div className="flex flex-col items-center justify-center bg-[#275C9D] py-[clamp(1rem,5vw,8rem)]">
+        <p className="font-bold text-white text-[clamp(1rem,3.5vw,6rem)]">
           Start Your Search.
         </p>
-        <p className="font-bold text-white text-[clamp(0.7rem,1.4vw,4rem)] py-[clamp(1rem,2vw,4rem)]">
+        <p className="font-bold text-white text-center text-[clamp(0.45rem,1.4vw,4rem)] py-[clamp(0.5rem,2vw,4rem)]">
           Choose an option for how you would like find your ESA rules.
         </p>
 
         <div className="py-[clamp(1rem,3vw,4rem)]">
-          <div className="flex gap-x-[clamp(1rem,3vw,3rem)]">
-            <button
-              className={blueTools}
-              onClick={() => router.push("/chat")}
-            >
+          <div className="flex gap-x-[clamp(1rem,3vw,5rem)]">
+            <button className={blueTools} onClick={goToChat}>
               CHAT
             </button>
-            <button
-              className={blueTools}
-              onClick={() => router.push("/map")}
-            >
+            <button className={blueTools} onClick={goToMap}>
               MAP
             </button>
-            <button
-              className={blueTools}
-              onClick={() => router.push("/manual-entry")}
-            >
+            <button className={blueTools} onClick={goToManualEntry}>
               MANUAL ENTRY
             </button>
           </div>
         </div>
-        <p className="font-bold text-[#9E9FA0] text-[clamp(0.5rem,0.8vw,0.8rem)]">
+        <p className="font-bold text-[#9E9FA0] text-[clamp(0.3rem,0.8vw,2rem)]">
           *Chat view is experimental and not the most accurate at this time*
         </p>
       </div>
