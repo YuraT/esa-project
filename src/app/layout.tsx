7a7980718a6da128c9797e768cd0bcf2f780725c
Eric Nguyen <eriicnguyen1@gmail.com>
2025-07-17 23:51:42 -0700

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,32 @@ const merriweatherSans = Merriweather_Sans({
   subsets: ["latin"],
 });
 
-
 export const metadata: Metadata = {
   title: "ESA Project",
   description: "Project to help with Endangered Species Act compliance",
+  applicationName: "EPA ESA",
+  manifest: "/manifest.json",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      {
+        url: "/favicon-96x96.png",
+        type: "image/png",
+        sizes: "96x96",
+      },
+      {
+        url: "/favicon-192x192.png",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        url: "/favicon-512x512.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
+    ],
+    apple: "/apple-icon.png",
+  },
 };
 
 export default function RootLayout({
@@ -26,6 +48,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="EPA ESA" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
+
       <body
         className={`${merriweatherSans.variable} ${geistMono.variable} antialiased`}
       >
