5e4c1f3fef32bf77eb69370a154a21d0ac49b3ad
Eric Nguyen <eriicnguyen1@gmail.com>
2025-07-21 03:14:56 -0700

    added back button

---
+++ src/app/testing/notification/page.tsx
@@ -0,0 +1,12 @@
+import BackButton from "@/app/components/BackButton";
+import Page from "../../utils/pwa";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <BackButton />
+      <Page />
+    </div>
+  );
+}
