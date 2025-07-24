5af87a15773ad31baffe5fcb8d8f53912bd0ba28
Eric Nguyen <eriicnguyen1@gmail.com>
2025-07-23 17:12:30 -0700

    Update BackButton.tsx

--- src/app/components/BackButton.tsx
+++ src/app/components/BackButton.tsx
@@ -3,19 +3,17 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { IoArrowBack } from "react-icons/io5";
 
-function BackButton() {
+export default function BackButton() {
   const router = useRouter();
 
   return (
-    <div>
+    <div className="bg-[#275C9D] p-[clamp(0.5rem,1.5vw,5rem)]">
       <button
-        className="flex items-center justify-center w-12 h-12 rounded-full bg-[#537BBA] transition-colors hover:bg-[#648fd1]"
+        className="flex items-center justify-center w-[clamp(1rem,3vw,7rem)] h-[clamp(1rem,3vw,7rem)] rounded-full bg-[#537BBA] transition-colors hover:bg-[#648fd1]"
         onClick={() => router.back()}
       >
-        <IoArrowBack className="text-white w-6 h-6" />
+        <IoArrowBack className="text-white w-[clamp(0.5rem,1.5vw,4rem)] h-[clamp(0.5rem,1.5vw,4rem)]" />
       </button>
     </div>
   );
 }
-
-export default BackButton;
