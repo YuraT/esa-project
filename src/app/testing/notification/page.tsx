854c908cf9e9fa72156ec9305b2d8f0da0ffe638
Eric Nguyen <eriicnguyen1@gmail.com>
2025-07-23 17:13:23 -0700

    added loading indicator to testing

--- src/app/testing/notification/page.tsx
+++ src/app/testing/notification/page.tsx
@@ -1,12 +1,14 @@
 import BackButton from "@/app/components/BackButton";
 import Page from "../../utils/pwa";
 import React from "react";
+import LoadingIndicator from "@/app/components/LoadingIndicator";
 
 export default function page() {
   return (
     <div>
       <BackButton />
       <Page />
+      <LoadingIndicator />
     </div>
   );
 }
