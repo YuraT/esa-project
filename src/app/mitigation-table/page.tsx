7d61225cf63ec21460da9ab41af2c19efdfd5403
Shakshi Sharma <shakshi.sharma805@gmail.com>
2025-08-04 21:35:28 -0700

    slight modifications, fixing back buttons

--- src/app/mitigation-table/page.tsx
+++ src/app/mitigation-table/page.tsx
@@ -9,21 +9,35 @@ import Step3 from "../components/Step3";
 import Step4 from "../components/Step4";
 import Step5 from "../components/Step5";
 import Step6 from "../components/Step6";
+import { useSearchParams, useRouter } from "next/navigation";
+
 
 export default function MitigationTable() {
 
     const [currentStep, setCurrentStep] = useState<number | null>(null);
+    const searchParams = useSearchParams();
+    const router = useRouter();
 
-    const scrollToStep = (id: string, stepNumber: number) => {
-        setCurrentStep(stepNumber);
+    const month = searchParams.get("month");
+    const product = searchParams.get("product");
+    const county = searchParams.get("county");
+
+    const scrollToStep = (id: string) => {
         document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    const handleNext = () => {
+        router.push(`/PrintReport?month=${encodeURIComponent(month || '')}&product=${encodeURIComponent(product || '')}&county=${encodeURIComponent(county || '')}`);
     };
 
     return (
         <div className="flex flex-col min-h-screen bg-white">
             {/*added header manually here due to positioning issue*/}
+
             <div className="w-full h-[15vh] flex items-center justify-center bg-[#275c9d]">
-                <div className="absolute left-10 flex items-center justify-center w-10 h-10 rounded-full bg-[#678dc9] cursor-pointer">
+                <div 
+                onClick={() => router.push('../map')} // <-- navigates to main page
+                className="absolute left-10 flex items-center justify-center w-10 h-10 rounded-full bg-[#678dc9] cursor-pointer">
                     <svg
                         className="w-4 h-4 text-white"
                         fill="none"
@@ -37,37 +51,47 @@ export default function MitigationTable() {
             </div>
             <div className="flex">
                 <div className="sticky top-0 flex flex-col items-center w-150 h-screen bg-[#cee0f5]">
-                    <div className="mt-18 mb-5 text-[#275c9d] text-2xl font-bold">
-                        Step {currentStep ?? "_"} of 6
-                    </div>
+                    <div className="mt-18 mb-5 text-[#275c9d] text-2xl font-bold">Step _ of 6</div>
 
-                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step1", 1)}>
-                        <div className="w-9 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">1</div>
+                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step1")}>
+                        <div className="w-9 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">
+                            1
+                        </div>
                         <div className="leading-tight ml-5 text-[#275c9d] font-semibold">County Pesticide Runoff Vulnerability</div>
                     </div>
 
-                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step2", 2)}>
-                        <div className="w-7 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">2</div>
+                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step2")}>
+                        <div className="w-7 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">
+                            2
+                        </div>
                         <div className="leading-tight ml-5 text-[#275c9d] font-semibold">Field Slope</div>
                     </div>
 
-                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step3", 3)}>
-                        <div className="w-7 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">3</div>
+                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step3")}>
+                        <div className="w-7 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">
+                            3
+                        </div>
                         <div className="leading-tight ml-5 text-[#275c9d] font-semibold">Predominantly Sandy Soils</div>
                     </div>
 
-                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step4", 4)}>
-                        <div className="w-7 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">4</div>
+                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step4")}>
+                        <div className="w-7 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">
+                            4
+                        </div>
                         <div className="leading-tight ml-5 text-[#275c9d] font-semibold">Mitigation Tracking</div>
                     </div>
 
-                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step5", 5)}>
-                        <div className="w-7 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">5</div>
+                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step5")}>
+                        <div className="w-7 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">
+                            5
+                        </div>
                         <div className="leading-tight ml-5 text-[#275c9d] font-semibold">Technical Specialist </div>
                     </div>
 
-                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step6", 6)}>
-                        <div className="w-7 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">6</div>
+                    <div className="flex items-center mt-10 bg-[#bed2e8] w-70 h-15 cursor-pointer" onClick={() => scrollToStep("step6")}>
+                        <div className="w-7 h-7 ml-4 rounded-full bg-[#577bb5] text-white flex items-center justify-center font-bold text-lg">
+                            6
+                        </div>
                         <div className="leading-tight ml-5 text-[#275c9d] font-semibold">Conservation Program </div>
                     </div>
 
@@ -91,6 +115,16 @@ export default function MitigationTable() {
                     <div id="step4"><Step4 /></div>
                     <div id="step5"><Step5 /></div>
                     <div id="step6"><Step6 /></div>
+
+                    <div className="mt-10 mb-10">
+                    <button
+                        onClick={handleNext}
+                        className="ml-220 bg-[#275c9d] text-white font-bold py-3 px-6 rounded-lg hover:bg-[#1f4b7a] transition duration-200"
+                    >
+                        Next
+                    </button>
+                </div>
+
                 </div>
             </div>
 
