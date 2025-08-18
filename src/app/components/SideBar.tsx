4a0080ff4b6925ab38fd4bf1d00510c96996ec94
Eric Nguyen <eriicnguyen1@gmail.com>
2025-08-18 10:01:05 -0700

    Update SideBar.tsx

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -14,12 +14,12 @@ export default function Sidebar({
 }: SidebarProps) {
   return (
     <div className="flex items-center flex-col w-[clamp(5rem,20vw,40rem)] min-h-screen bg-[#DAE8F8]">
-      <div className="pt-[clamp(1rem,3vw,5rem)]">
-        <p className="text-[clamp(0.5rem,3vw,3rem)] text-[#375B85] font-bold">
+      <div className="p-[clamp(1rem,3vw,5rem)]">
+        <p className="text-[clamp(0.5rem,2vw,3rem)] text-[#375B85] font-bold">
           Step {currentStep} of {steps.length}
         </p>
       </div>
-      <div className="pt-[clamp(0.5rem,1vw,3rem)]">
+      <div className="pt-[clamp(0.5rem,1vw,2rem]">
         <ol>
           {steps.map((step, index) => (
             <li
@@ -31,11 +31,11 @@ export default function Sidebar({
                   : "hover:bg-[#CEE0F5]"
               }`}
             >
-              <div className="flex items-center my-[clamp(0.4rem,2vw,5rem)]">
-                <div className="flex items-center justify-center bg-[#537BBA] text-white text-[clamp(0.3rem,1vw,2rem)] font-extrabold w-[clamp(0.6rem,3vw,3rem)] h-[clamp(0.6rem,3vw,3rem)] rounded-full">
+              <div className="flex items-center mb-[clamp(0.4rem,2vw,3rem)]">
+                <div className="flex items-center justify-center bg-[#537BBA] text-white text-[clamp(0.3rem,1.3vw,3rem)] font-extrabold w-[clamp(0.6rem,3vw,3rem)] h-[clamp(0.6rem,3vw,3rem)] rounded-full">
                   {index + 1}
                 </div>
-                <div className="font-bold w-[clamp(3.9rem,15vw,30rem)] p-[clamp(0.3rem,1vw,3rem)] text-[clamp(0.3rem,1vw,2rem)]">
+                <div className="flex items-center font-bold w-[clamp(3.9rem,15vw,30rem)] h-[clamp(1rem,8vw,11rem)] p-[clamp(0.3rem,1vw,3rem)] text-[clamp(0.3rem,1.3vw,2rem)]">
                   {step}
                 </div>
               </div>
